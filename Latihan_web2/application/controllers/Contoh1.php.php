58f8a93f03909aa84fd69c7c438ed15fc430289b
yanramdani <yanramdan17@gmail.com>
2021-11-24 12:14:35 +0700

    Tugas pertemuan 9

---
+++ Latihan_web2/application/controllers/Contoh1.php.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controler
+{
+    public function index()
+    {
+        echo "<h1> Perkenalkan</h1>";
+        echo"Nama Saya M. Rian Ramdani
+        saya tinggal di daerah puncak bogor
+        olahraga yang saya sukai adalah
+        bersepeda"
+    }
+}
